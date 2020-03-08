c151f992ae90dc6fa022233cfe80b8ac01d17e37
ranaaditya <mail2aditya.rana98@gmail.com>
2020-03-08 14:08:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/ranaaditya/processing_app/ProcessingMainActivity.java
@@ -0,0 +1,14 @@
+package com.ranaaditya.processing_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProcessingMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
